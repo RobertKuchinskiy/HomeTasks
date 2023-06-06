169df173cf4624fc49c169ff1817d8298ed93738
RobertKuchinskiy <134945392+RobertKuchinskiy@users.noreply.github.com>
2023-06-06 19:36:54 +0300

    Update type_variables.js

---
+++ type_variables.js
@@ -0,0 +1,62 @@
+//Домашнее задание №4
+
+let typeString = 'привет'
+let typeBoolean =  true
+let typeNumber  = 23
+let typeBoolean2 = false
+
+//Сложение
+
+console.log(typeString + typeBoolean)
+console.log(typeString + typeNumber)
+console.log(typeNumber + typeBoolean)
+console.log(typeBoolean2 + typeString)
+console.log(typeBoolean2 + typeNumber)
+
+//Умножение 
+
+console.log(typeString * typeBoolean)
+console.log(typeString * typeNumber)
+console.log(typeNumber * typeBoolean)
+console.log(typeBoolean2 * typeString)
+console.log(typeBoolean2 * typeNumber)
+
+//Деление 
+
+console.log(typeString / typeBoolean)
+console.log(typeString / typeNumber)
+console.log(typeNumber / typeBoolean)
+console.log(typeString / typeBoolean2)
+console.log(typeNumber / typeBoolean2)
+console.log(typeNumber / typeBoolean)
+console.log(typeBoolean2 / typeNumber)
+console.log(typeBoolean2 / typeString)
+console.log(typeBoolean / typeString)
+console.log(typeBoolean / typeNumber)
+
+//Преобразование
+
+console.log(Boolean("123"))
+console.log(Boolean("123a"))
+console.log(Boolean("abc"))
+console.log(Boolean(true))
+console.log(Boolean(false))
+console.log(Boolean(null))
+console.log(Boolean())
+console.log(Boolean(1))
+
+console.log(Number("456"))
+console.log(Number("456b"))
+console.log(Number("def"))
+console.log(Number(true))
+console.log(Number(false))
+console.log(Number(null))
+console.log(Number())
+
+console.log(String("789"))
+console.log(String("789c"))
+console.log(String("ghi"))
+console.log(String(true))
+console.log(String(false))
+console.log(String(null))
+console.log(String())
