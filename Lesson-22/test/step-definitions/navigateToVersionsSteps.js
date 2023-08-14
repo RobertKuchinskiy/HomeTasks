123f714e5a794c795902af44a998c3eb357b75ce
RobertKuchinskiy <134945392+RobertKuchinskiy@users.noreply.github.com>
2023-08-14 20:17:58 +0000

    third commit

--- Lesson-22/test/step-definitions/navigateToVersionsSteps.js
+++ Lesson-22/test/step-definitions/navigateToVersionsSteps.js
@@ -2,22 +2,28 @@ const { Given, When, Then } = require('cucumber');
 const { expect } = require('chai');
 const VersionPage = require('../pageobjects/VersionPage');
 
-Given('I am on the website', async () => {
-  VersionPage.open();
+Given(/^I open the website$/, () => {
+    VersionPage.open();
 });
 
-When('I navigate to the versions page', async () => {
-  VersionPage.getStartedButton.click();
-  VersionPage.oldDocsLink.waitForDisplayed();
-  VersionPage.oldDocsLink.click();
+When(/^I navigate to the old versions$/, async () => {
+    VersionPage.openOldVersions();
+
+    browser.pause(2000);
+
+    const currentTitle = await VersionPage.getTitle();
+    console.log('Current Title:', currentTitle);
 });
 
-Then('I should see the title {string}', async (expectedTitle) => {
+Then(/^I should see the title "([^"]*)"$/, async (expectedTitle) => {
   await browser.waitUntil(async () => {
     const currentTitle = await VersionPage.getTitle();
     return currentTitle === expectedTitle;
-  }, { timeout: 10000, timeoutMsg: 'Expected title to be ' + expectedTitle });
+}, {
+    timeout: 20000, 
+    timeoutMsg: `Expected title to be ${expectedTitle}`
+});
 
-  const currentTitle = await VersionPage.getTitle();
-  expect(currentTitle).to.equal(expectedTitle);
+    const currentTitle = await VersionPage.getTitle();
+    expect(currentTitle).to.equal(expectedTitle);
 });
